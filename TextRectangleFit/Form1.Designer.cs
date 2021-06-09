2ba4747a4d16e239299a050350011f09f68ae651
PeterNespesny <PeterNespesny@PeterNespesny.PeterNespesny>
2021-06-09 13:48:56 +0300

    Initial

---
+++ TextRectangleFit/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace TextRectangleFit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BT_Generate = new System.Windows.Forms.Button();
+            this.PB_Pic = new System.Windows.Forms.PictureBox();
+            this.RTB_Text = new System.Windows.Forms.RichTextBox();
+            this.NUD_Padding = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LB_MaxRows = new System.Windows.Forms.Label();
+            this.CB_Double = new System.Windows.Forms.CheckBox();
+            this.CB_LineBreak = new System.Windows.Forms.CheckBox();
+            this.BT_UseInputFromRTB = new System.Windows.Forms.Button();
+            this.LB_Rect = new System.Windows.Forms.Label();
+            this.CB_DeleteLargeWords = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_Pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NUD_Padding)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BT_Generate
+            // 
+            this.BT_Generate.Location = new System.Drawing.Point(12, 12);
+            this.BT_Generate.Name = "BT_Generate";
+            this.BT_Generate.Size = new System.Drawing.Size(97, 29);
+            this.BT_Generate.TabIndex = 0;
+            this.BT_Generate.Text = "Generate";
+            this.BT_Generate.UseVisualStyleBackColor = true;
+            this.BT_Generate.Click += new System.EventHandler(this.BT_Generate_Click);
+            // 
+            // PB_Pic
+            // 
+            this.PB_Pic.Image = global::TextRectangleFit.Properties.Resources._5xlQJRVbfoo___Copy;
+            this.PB_Pic.Location = new System.Drawing.Point(0, 0);
+            this.PB_Pic.Name = "PB_Pic";
+            this.PB_Pic.Size = new System.Drawing.Size(600, 793);
+            this.PB_Pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.PB_Pic.TabIndex = 1;
+            this.PB_Pic.TabStop = false;
+            this.PB_Pic.Paint += new System.Windows.Forms.PaintEventHandler(this.PB_Pic_Paint);
+            // 
+            // RTB_Text
+            // 
+            this.RTB_Text.Location = new System.Drawing.Point(58, 684);
+            this.RTB_Text.Name = "RTB_Text";
+            this.RTB_Text.Size = new System.Drawing.Size(489, 96);
+            this.RTB_Text.TabIndex = 2;
+            this.RTB_Text.Text = "";
+            // 
+            // NUD_Padding
+            // 
+            this.NUD_Padding.Location = new System.Drawing.Point(67, 47);
+            this.NUD_Padding.Name = "NUD_Padding";
+            this.NUD_Padding.Size = new System.Drawing.Size(42, 20);
+            this.NUD_Padding.TabIndex = 3;
+            this.NUD_Padding.ValueChanged += new System.EventHandler(this.NUD_Padding_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Padding:";
+            // 
+            // LB_MaxRows
+            // 
+            this.LB_MaxRows.AutoSize = true;
+            this.LB_MaxRows.Location = new System.Drawing.Point(12, 76);
+            this.LB_MaxRows.Name = "LB_MaxRows";
+            this.LB_MaxRows.Size = new System.Drawing.Size(51, 13);
+            this.LB_MaxRows.TabIndex = 4;
+            this.LB_MaxRows.Text = "max rows";
+            // 
+            // CB_Double
+            // 
+            this.CB_Double.AutoSize = true;
+            this.CB_Double.Location = new System.Drawing.Point(115, 19);
+            this.CB_Double.Name = "CB_Double";
+            this.CB_Double.Size = new System.Drawing.Size(37, 17);
+            this.CB_Double.TabIndex = 5;
+            this.CB_Double.Text = "x2";
+            this.CB_Double.UseVisualStyleBackColor = true;
+            // 
+            // CB_LineBreak
+            // 
+            this.CB_LineBreak.AutoSize = true;
+            this.CB_LineBreak.Checked = true;
+            this.CB_LineBreak.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.CB_LineBreak.Location = new System.Drawing.Point(158, 19);
+            this.CB_LineBreak.Name = "CB_LineBreak";
+            this.CB_LineBreak.Size = new System.Drawing.Size(76, 17);
+            this.CB_LineBreak.TabIndex = 5;
+            this.CB_LineBreak.Text = "Line break";
+            this.CB_LineBreak.UseVisualStyleBackColor = true;
+            // 
+            // BT_UseInputFromRTB
+            // 
+            this.BT_UseInputFromRTB.Location = new System.Drawing.Point(58, 649);
+            this.BT_UseInputFromRTB.Name = "BT_UseInputFromRTB";
+            this.BT_UseInputFromRTB.Size = new System.Drawing.Size(97, 29);
+            this.BT_UseInputFromRTB.TabIndex = 0;
+            this.BT_UseInputFromRTB.Text = "Use as input";
+            this.BT_UseInputFromRTB.UseVisualStyleBackColor = true;
+            this.BT_UseInputFromRTB.Click += new System.EventHandler(this.BT_UseInputFromRTB_Click);
+            // 
+            // LB_Rect
+            // 
+            this.LB_Rect.AutoSize = true;
+            this.LB_Rect.Location = new System.Drawing.Point(12, 98);
+            this.LB_Rect.Name = "LB_Rect";
+            this.LB_Rect.Size = new System.Drawing.Size(47, 13);
+            this.LB_Rect.TabIndex = 4;
+            this.LB_Rect.Text = "msg rect";
+            // 
+            // CB_DeleteLargeWords
+            // 
+            this.CB_DeleteLargeWords.AutoSize = true;
+            this.CB_DeleteLargeWords.Location = new System.Drawing.Point(158, 42);
+            this.CB_DeleteLargeWords.Name = "CB_DeleteLargeWords";
+            this.CB_DeleteLargeWords.Size = new System.Drawing.Size(210, 17);
+            this.CB_DeleteLargeWords.TabIndex = 5;
+            this.CB_DeleteLargeWords.Text = "Delete large words (line break required)";
+            this.CB_DeleteLargeWords.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 792);
+            this.Controls.Add(this.CB_DeleteLargeWords);
+            this.Controls.Add(this.CB_LineBreak);
+            this.Controls.Add(this.CB_Double);
+            this.Controls.Add(this.LB_Rect);
+            this.Controls.Add(this.LB_MaxRows);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NUD_Padding);
+            this.Controls.Add(this.RTB_Text);
+            this.Controls.Add(this.BT_UseInputFromRTB);
+            this.Controls.Add(this.BT_Generate);
+            this.Controls.Add(this.PB_Pic);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "TextRectangleFit";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PB_Pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NUD_Padding)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button BT_Generate;
+        private System.Windows.Forms.PictureBox PB_Pic;
+        private System.Windows.Forms.RichTextBox RTB_Text;
+        private System.Windows.Forms.NumericUpDown NUD_Padding;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LB_MaxRows;
+        private System.Windows.Forms.CheckBox CB_Double;
+        private System.Windows.Forms.CheckBox CB_LineBreak;
+        private System.Windows.Forms.Button BT_UseInputFromRTB;
+        private System.Windows.Forms.Label LB_Rect;
+        private System.Windows.Forms.CheckBox CB_DeleteLargeWords;
+    }
+}
+
---
+++ TextRectangleFit/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextRectangleFit
+{
+    public struct SplitWord
+    {
+        public string PartThatFits;
+        public string Remainder;
+    }
+
+    public partial class Form1 : Form
+    {
+        Rectangle _MsgRect = new Rectangle(56, 153, 159, 280);
+        int _Padding = 7;
+
+        string[] _SourceSplitByPeriods = File.ReadAllText("source.txt", Encoding.UTF8).Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+        Random _RNG = new Random();
+        string _Msg = "TEST";
+
+        int _MinFrontSize = 14;
+        int _WidthToFit => _MsgRect.Width - _Padding * 2;
+
+        int _MaxRows;
+
+        Font _Impact;
+        StringFormat _SF;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _Impact = new Font("Impact", _MinFrontSize);
+            _SF = new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center };
+
+            int singleRowHeight;
+            using (Graphics graphics = Graphics.FromImage(new Bitmap(1, 1)))
+            {
+                singleRowHeight = Convert.ToInt32(graphics.MeasureString("ss", _Impact).Height); ;
+                _MaxRows = _MsgRect.Height / singleRowHeight;
+            }
+
+            LB_MaxRows.Text = $"Max rows: {_MaxRows}, Single row Height: {singleRowHeight}";
+            LB_Rect.Text = $"Msg rect:" + _MsgRect.ToString();
+
+            NUD_Padding.Value = _Padding;
+
+            BT_Generate.PerformClick();
+        }
+        private void BT_Generate_Click(object sender, EventArgs e)
+        {
+            _Msg = GenerateMessageFromTextFile();
+            PB_Pic.Invalidate();
+        }
+        private string GenerateMessageFromTextFile()
+        {
+            return GenerateMessage(_SourceSplitByPeriods[_RNG.Next(0, _SourceSplitByPeriods.Length)]);
+        }
+        private string GenerateMessage(string source)
+        {
+            if (String.IsNullOrWhiteSpace(source))
+                return "null";
+
+            string initialMsg = source.Replace("\n", "");
+
+            if (CB_Double.Checked)
+                initialMsg += initialMsg;
+
+            RTB_Text.Text = initialMsg;
+            Text = $"Initial Words: {GetWordCount(initialMsg)}";
+
+            string newMsg = "";
+
+            if (!CB_LineBreak.Checked)
+            {
+                for (int startIndex = 0, length = 1, rows = 1; rows <= _MaxRows;)
+                {
+                    string subString = initialMsg.Substring(startIndex, length);
+                    if (startIndex + length == initialMsg.Length)
+                    {
+                        newMsg += subString;
+                        break;
+                    }
+
+                    string subStringNext = initialMsg.Substring(startIndex, length + 1);
+                    if (DoesStringFit(subString))
+                    {
+                        if (DoesStringFit(subStringNext))
+                        {
+                            length++;
+                            continue;
+                        }
+
+                        newMsg += subString + "\n";
+                        rows++;
+                        startIndex += length;
+                        length = 1;
+                    }
+                    else
+                    {
+                        throw new Exception("Single symbol is larger than max width!");
+                    }
+                }
+            }
+            else //with line break 
+            {
+                List<string> wordList = initialMsg.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                for (int startIndex = 0, length = 1, rows = 1; newMsg.Split('\n').Length <= _MaxRows;)
+                {
+                    string subString = GetStringFromWordList(startIndex, length, wordList);
+                    if (startIndex + length == wordList.Count)
+                    {
+                        if (DoesStringFit(subString))
+                            newMsg += subString;
+                        else
+                            goto SubStringDoesntFit;
+                        break;
+                    }
+
+                    string subStringNext = GetStringFromWordList(startIndex, length + 1, wordList);
+                    if (DoesStringFit(subString))
+                    {
+                        if (DoesStringFit(subStringNext))
+                        {
+                            length++;
+                            continue;
+                        }
+
+
+                        newMsg += subString + "\n";
+                        rows++;
+                        startIndex += length;
+                        length = 1;
+                        continue;
+                    }
+
+                SubStringDoesntFit: //length == 1
+                    if (CB_DeleteLargeWords.Checked)
+                        wordList.RemoveAt(startIndex);
+                    else
+                    {
+                        var splitWord = GetSplitWord(subString);
+                        wordList[startIndex] = splitWord.PartThatFits;
+                        wordList.Insert(startIndex + 1, splitWord.Remainder);
+                    }
+                }
+            }
+
+            newMsg = newMsg.Trim();
+            Text += $", Actual Rows: {newMsg.Split('\n').Length}";
+            return newMsg;
+        }
+        private SplitWord GetSplitWord(string word)
+        {
+            using (Graphics g = Graphics.FromImage(new Bitmap(1, 1)))
+            {
+                string half = word.Substring(0, word.Length / 2);
+
+                while (!DoesStringFit(half))
+                    half = half.Substring(0, half.Length / 2);
+
+                int length = 1;
+                while (DoesStringFit(half + word.Substring(half.Length, length)))
+                    length++;
+
+                return new SplitWord()
+                {
+                    PartThatFits = word.Substring(0, half.Length + length - 1),
+                    Remainder = word.Substring(half.Length + length - 1)
+                };
+            }
+        }
+        private bool DoesStringFit(string str)
+        {
+            using (Graphics g = Graphics.FromImage(new Bitmap(1, 1)))
+            {
+                return g.MeasureString(str, _Impact).Width < _WidthToFit;
+            }
+        }
+        private string GetStringFromWordList(int startIndex, int length, List<string> list)
+        {
+            return string.Join(" ", list.GetRange(startIndex, length));
+        }
+        private int GetWordCount(string msg)
+        {
+            int wordCount = 0, index = 0;
+
+            // skip whitespace until first word
+            while (index < msg.Length && char.IsWhiteSpace(msg[index]))
+                index++;
+
+            while (index < msg.Length)
+            {
+                // check if current char is part of a word
+                while (index < msg.Length && !char.IsWhiteSpace(msg[index]))
+                    index++;
+
+                wordCount++;
+
+                // skip whitespace until next word
+                while (index < msg.Length && char.IsWhiteSpace(msg[index]))
+                    index++;
+            }
+
+            return wordCount;
+        }
+        private void PB_Pic_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawRectangle(Pens.Red, _MsgRect);
+            e.Graphics.DrawString(_Msg, _Impact, Brushes.Black, _MsgRect, _SF);
+        }
+        private void NUD_Padding_ValueChanged(object sender, EventArgs e)
+        {
+            _Padding = Convert.ToInt32(NUD_Padding.Value);
+        }
+        private void BT_UseInputFromRTB_Click(object sender, EventArgs e)
+        {
+            _Msg = GenerateMessage(RTB_Text.Text);
+            PB_Pic.Invalidate();
+        }
+    }
+}
